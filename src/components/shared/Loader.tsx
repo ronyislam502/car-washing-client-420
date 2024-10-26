79cfe894fcb52b5d42b49bfa2ee31af3e166f7fc
ronyislam502 <ronyislam502@gmail.com>
2024-10-26 19:03:15 +0600

    loader

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,11 @@
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div>
+        <div className="loader"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
